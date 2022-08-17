b9ee324c03a68f0771d79f6441021432f9b1d4b1
alexhernandezgarcia <alexhdezgarcia@gmail.com>
2022-08-17 15:48:45 -0400

    pseudocode for new al main

---
+++ activelearning.py
@@ -0,0 +1,8 @@
+# Get initial data set
+for it in range(n_iter):
+    # 1. Train proxy
+    # 2. Train GFN
+    # 3. Sample queries from GFN
+    # 4. Get scores for queries from oracle
+    # 5. Augment data set
+    pass
