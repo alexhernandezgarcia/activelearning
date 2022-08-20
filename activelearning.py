b223425ccd3ba47a7deda1d170cb41053e9956c1
Bao Nguyen <manhbaonguyen5@gmail.com>
2022-08-20 18:37:43 -0400

    added big components, removed black

--- activelearning.py
+++ activelearning.py
@@ -1,8 +1,90 @@
-# Get initial data set
-for it in range(n_iter):
-    # 1. Train proxy
-    # 2. Train GFN
-    # 3. Sample queries from GFN
-    # 4. Get scores for queries from oracle
-    # 5. Augment data set
-    pass
+from omegaconf import DictConfig, OmegaConf
+import hydra
+
+#TODO : instantiate the config with hydra . Once we have the config object we can pass it to other functions
+class ActiveLearning:
+    def __init__(self, config):
+        self.config = OmegaConf.load(config)
+        
+        self.oracle = Oracle(self.config)
+        self.proxy = Proxy(self.config)
+        self.acq = AcquisitionFunction(self.config, self.proxy)
+        self.env = Env(self.config, self.acq)
+        self.gflownet = GFlowNet(self.config, self.env)
+        self.querier = Querier(self.config, self.gflownet)
+
+    
+    def runPipeline(self):
+        self.iter = None
+        self.terminal = 0
+
+        #we initialize the first dataset
+        self.oracle.initializeDataset()
+
+        #we run each round of active learning
+        for self.iter in range(self.config.al.n_iter):
+            if self.iter == (self.config.al.n_iter - 1):
+                self.terminal = 1
+            self.iterate()
+    
+    def iterate(self):
+        self.proxy.converge()
+        self.gflownet.train()
+
+        if self.terminal == 0:
+            queries = self.querier.buildQuery()
+            energies = self.oracle.score(queries)
+            self.oracle.updateDataset(queries, energies)
+
+
+
+
+
+
+
+
+class Oracle:
+    def __init__(self, config):
+        self.config = config
+
+    def initializeDataset(self):
+        return
+    
+    def score(self, queries):
+        return
+
+    def updateDataset(self, queries, energies):
+        return
+
+class Proxy:
+    def __init__(self, config):
+        self.config = config
+    
+    def converge(self):
+        return
+
+class AcquisitionFunction:
+    def __init__(self, config, proxy):
+        self.config = config
+        self.proxy = proxy
+
+class Env:
+    def __init__(self, config, acq):
+        self.config = config
+        self.acq = acq
+
+class GFlowNet:
+    def __init__(self, config, env):
+        self.config = config
+        self.env = env
+    
+    def train(self):
+        return
+
+class Querier:
+    def __init__(self, config, gflownet):
+        self.config = config
+        self.gflownet = gflownet
+    
+    def buildQuery(self):
+        return
