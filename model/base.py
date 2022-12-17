7aa6ed8511cbfd0dff888eb4c03853adfe282878
nikita-0209 <nikitasaxena0209@gmail.com>
2022-12-17 14:28:01 -0500

    create dropout regressor and corresponding mlp class

--- model/base.py
+++ model/base.py
@@ -1,51 +1,54 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.nn.utils.rnn import pad_sequence
 import numpy as np
-import random
-import math
-import copy
 import os
-from abc import abstractmethod
 from torch.optim import Adam
+import hydra
 
-class Model(nn.Module):
-    def __init__(self, config, logger):
+ACTIVATION_KEY = {'tanh': nn.Tanh(), 'relu': nn.ReLU(), 'sigmoid': nn.Sigmoid(), 'leaky_relu': nn.LeakyReLU()}
+
+class DropoutRegressor(nn.Module):
+    def __init__(self, config, env, dataset, logger):
+        """
+        Args:
+            config specific to the surrogate model
+            dataset class which has function to get dataloaders
+            logger
+
+        Inialises model and optimiser. Fits the model and saves it once convergence is reached.
+        """
         self.config = config
         self.logger = logger
 
         self.device = self.config.device
-        self.path_data = self.config.path.data_oracle
-        self.path_model = self.config.path.model_proxy
+        self.path_model = self.config.path_model
 
         # Training Parameters
-        self.training_eps = self.config.proxy.training.eps
-        self.max_epochs = self.config.proxy.training.max_epochs
-        self.history = self.config.proxy.training.history
+        self.training_eps = self.config.training.eps
+        self.max_epochs = self.config.training.max_epochs
+        self.history = self.config.training.history
         assert self.history <= self.max_epochs
-        self.dropout = self.config.proxy.training.dropout
-        self.batch_size = self.config.proxy.training.training_batch
+        self.batch_size = self.config.training.training_batch
 
         # Dataset management
+        self.dataset = dataset
         self.shuffle_data = self.config.proxy.data.shuffle
         self.seed_data = self.config.proxy.data.seed
 
-        self.model_class = NotImplemented  # will be precised in child classes
 
-    @abstractmethod
     def init_model(self):
         """
-        Initialize the proxy we want (cf config). Each possible proxy is a class (MLP, transformer ...)
-        Ensemble methods will be another separate class
+        Initialize the network (MLP, Transformer, RNN)
         """
-        self.model = self.model_class(self.config).to(self.device)
-        self.optimizer = optim.AdamW(self.model.parameters(), amsgrad=True)
+        self.model = self.model_class = hydra.utils.instantiate(self.config.proxy, env).to(self.device)
+        self.optimizer = Adam(
+            self.model.parameters(), self.config.learning_rate, self.config.weight_decay
+        )
 
-    @abstractmethod
     def load_model(self, dir_name=None):
         """
-        will not have to be used normally because the global object ActiveLearning.proxy will be continuously updated
+        Load and returns the model
         """
         if dir_name == None:
             dir_name = self.config.path.model_proxy
@@ -56,22 +59,31 @@ class Model(nn.Module):
             checkpoint = torch.load(dir_name)
             self.model.load_state_dict(checkpoint["model_state_dict"])
             self.optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
-
-            if self.device == "cuda":
-                self.model.cuda()  # move net to GPU
-                for state in self.optimizer.state.values():  # move optimizer to GPU
-                    for k, v in state.items():
-                        if isinstance(v, torch.Tensor):
-                            state[k] = v.cuda()
-
-        # TODO: return self.model so that its easily integrated with proxy model
+            self.model.to(self.device)
+            for state in self.optimizer.state.values():  # move optimizer to GPU
+                for k, v in state.items():
+                    if isinstance(v, torch.Tensor):
+                            state[k] = v.to(self.device)
+            return self.model
         else:
-            raise NotImplementedError
+            raise FileNotFoundError
 
-    @abstractmethod
-    def converge(self, data_handler):
+    def save_model(self):
         """
-        will call getDataLoaders/train_batch/ test / checkConvergencce
+        Saves model once convergence is attained.
+        """
+        torch.save(
+            {
+                "model_state_dict": self.model.state_dict(),
+                "optimizer_state_dict": self.optimizer.state_dict(),
+            },
+            self.path_model,
+        )
+
+    def fit(self):
+        """
+        Initlaises the model and dataloaders.
+        Trains the model and saves it once convergence is attained.
         """
         # we reset the model, cf primacy bias, here we train on more and more data
         self.init_model()
@@ -80,7 +92,7 @@ class Model(nn.Module):
         [self.err_tr_hist, self.err_te_hist] = [[], []]
 
         # get training data in torch format
-        tr, te = data_handler.get_data_loaders()
+        train_loader, test_loader = self.dataset.get_data_loaders()
 
         self.converged = 0
         self.epochs = 0
@@ -90,22 +102,15 @@ class Model(nn.Module):
             if (
                 self.epochs > 0
             ):  #  this allows us to keep the previous model if it is better than any produced on this run
-                self.train(tr)  # already appends to self.err_tr_hist
+                self.train(train_loader)  # already appends to self.err_tr_hist
             else:
                 self.err_tr_hist.append(0)
 
-            self.test(te)
-
+            self.test(test_loader)
             if self.err_te_hist[-1] == np.min(
                 self.err_te_hist
             ):  # if this is the best test loss we've seen
-                torch.save(
-                    {
-                        "model_state_dict": self.model.state_dict(),
-                        "optimizer_state_dict": self.optimizer.state_dict(),
-                    },
-                    self.path_model,
-                )  # we update only the best, not keep the previous ones
+                self.save_model()
                 print(
                     "new best found at epoch {}, of value {:.4f}".format(
                         self.epochs, self.err_te_hist[-1]
@@ -131,15 +136,16 @@ class Model(nn.Module):
             #         self.err_tr_hist, self.err_te_hist
             #     )
 
-    @abstractmethod
     def train(self, tr):
         """
-        will call getLoss
+        Args: 
+            train-loader
         """
         err_tr = []
         self.model.train(True)
-        for i, trainData in enumerate(tr):
-            loss = self.get_loss(trainData)
+        for x_batch, y_batch in enumerate(tr):
+            output = self.model(x_batch)
+            loss = F.mse_loss(output[:, 0], y_batch.float())
             self.logger.log_metric("proxy_train_mse", loss.item())
             err_tr.append(loss.data)
             self.optimizer.zero_grad()
@@ -148,33 +154,17 @@ class Model(nn.Module):
 
         self.err_te_hist.append(torch.mean(torch.stack(err_tr)).cpu().detach().numpy())
 
-    @abstractmethod
     def test(self, te):
         err_te = []
         self.model.eval()
         with torch.no_grad():
-            for i, testData in enumerate(te):
-                loss = self.get_loss(testData)
+            for x_batch, y_batch in enumerate(te):
+                output = self.model(x_batch)
+                loss = F.mse_loss(output[:, 0], y_batch.float())
                 self.logger.log_metric("proxy_val_mse", loss.item())
                 err_te.append(loss.data)
-
         self.err_te_hist.append(torch.mean(torch.stack(err_te)).cpu().detach().numpy())
 
-    @abstractmethod
-    def get_loss(self, data):
-        inputs = data[0]
-        targets = data[1]
-        inputs = inputs.to(self.device)
-        targets = targets.to(self.device)
-        input_lens = data[2]
-        inputs = inputs.to(self.device)
-        input_lens = input_lens.to(self.device)
-        targets = targets.to(self.device)
-        output = self.model(inputs, input_lens, targets)
-        loss = F.mse_loss(output[:, 0], targets.float())
-        return loss
-
-    @abstractmethod
     def check_convergence(self):
         eps = self.training_eps
         history = self.history
@@ -210,9 +200,10 @@ class Model(nn.Module):
                 )
             )
 
-    @abstractmethod
-    def evaluate(self, data):
-        self.model.eval()
+    def forward_with_uncertainty(self, x):
+        self.model.train()
         with torch.no_grad():
-            output = self.model(data).cpu().detach().numpy()
-            return output
+            outputs = torch.cat(
+                [self.forward(x) for _ in range(self.config.num_dropout_samples)]
+            )
+        return outputs.mean(dim=0), outputs.std(dim=0)
--- model/mlp.py
+++ model/mlp.py
@@ -1,33 +1,64 @@
-from model.base import Model
+import torch.nn as nn
+from base import ACTIVATION_KEY
+import torch
 
-class MLP(Model):
-    def __init__(self, config, logger, init_model=False):
-        super().__init__(config, logger)
-        self.model_class = MLP
-        self.device = config.device
-        if init_model:
-            self.init_model()
 
-    def init_model(self):
-        super().init_model()
+class MLP(nn.Module):
+    def __init__(self, config, env, transformerCall=False):
+        super(MLP, self).__init__()
+        """
+        Args:
+            configuration specific to surrogate model
+            env (required for specifying input dimensions of model)
 
-    def load_model(self, dir_name=None):
-        super().load_model(dir_name)
+        Initlaise the model given the parameters defined in the config
+        """
+        self.config = config
+        self.activation = ACTIVATION_KEY[config.activation]
 
-    def converge(self, data_handler):
-        super().converge(data_handler)
+        self.input_max_length = env.max_len
+        self.input_classes = env.dict_size
+        self.out_dim = self.config.num_output
 
-    def train(self, tr):
-        super().train(tr)
+        if transformerCall == False:
+            self.hidden_layers = self.config.hidden_layer
+            self.dropout_prob = self.config.dropout_prob
+            self.init_layer_depth = int((self.input_classes) * (self.input_max_length))
 
-    def test(self, te):
-        super().test(te)
+        else:
+            # this clause is entered only when transformer specific config is passed
+            self.hidden_layers = self.config.mlp.hidden_layers
+            self.dropout_prob = self.config.mlp.dropout_prob
+            self.init_layer_depth = self.config.mlp.in_dim
 
-    def get_loss(self, data):
-        return super().get_loss(data)
-
-    def check_convergence(self):
-        super().check_convergence()
-
-    def evaluate(self, data):
-        return super().evaluate(data)
+        layers = [
+            nn.Linear(self.init_layer_depth, self.hidden_layers[0]),
+            self.activation,
+        ]
+        layers += [nn.Dropout(self.dropout_prob)]
+        for i in range(1, len(self.hidden_layers)):
+            layers.extend(
+                [
+                    nn.Linear(self.hidden_layers[i - 1], self.hidden_layers[i]),
+                    self.activation,
+                    nn.Dropout(self.dropout_prob),
+                ]
+            )
+        layers.append(nn.Linear(self.hidden_layers[-1], self.out_dim))
+        self.model = nn.Sequential(*layers)
+    
+    def forward(self, x):
+        """
+        Args:
+            tensor of dimension batch x max_seq_length_in_batch x k, 
+            where k is num_tokens if input is required to be one-hot encoded
+        
+        """
+        # Pads the tensor till maximum length of dataset
+        input = torch.zeros(x.shape[0], self.input_max_length, self.input_classes)
+        input[:, : x.shape[1], :] = x
+        # Reshapes the tensor to batch_size x init_layer_depth
+        x = input.reshape(x.shape[0], -1)
+        # Performs a forward call
+        return self.model(x)
+        
