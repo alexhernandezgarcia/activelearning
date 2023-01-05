68942b6529784eeb2bafa3947a8e5ea6952cc80d
nikita-0209 <nikitasaxena0209@gmail.com>
2023-01-05 14:08:29 -0500

    add botorch based ucb for sanity check purposes

---
+++ proxy/botorch_models.py
@@ -0,0 +1,49 @@
+import torch
+from botorch.models.model import Model
+from gpytorch.distributions import MultivariateNormal, MultitaskMultivariateNormal
+from botorch.posteriors.gpytorch import GPyTorchPosterior
+import os
+
+
+class ProxyBotorchUCB(Model):
+    def __init__(self, regressor, num_dropout_samples):
+        super().__init__()
+        self.regressor = regressor
+        self._num_outputs = 1
+        self.num_dropout_samples = num_dropout_samples
+
+    def posterior(self, X, observation_noise=False, posterior_transform=None):
+        super().posterior(X, observation_noise, posterior_transform)
+
+        self.regressor.model.train()
+        dim = X.ndim
+        if dim == 3:
+            X = X.squeeze(-2)
+
+        with torch.no_grad():
+            mean, _, var = self.regressor.forward_with_uncertainty(
+                X, self.num_dropout_samples
+            )
+        mean = mean.unsqueeze(-1)
+        var = var.unsqueeze(-1)
+        # if var is an array of zeros then we add a small value to it
+        var = torch.where(var == 0, torch.ones_like(var) * 1e-4, var)
+
+        covar = [torch.diag(var[i]) for i in range(X.shape[0])]
+        covar = torch.stack(covar, axis=0)
+        mvn = MultivariateNormal(mean, covar)
+        posterior = GPyTorchPosterior(mvn)
+        return posterior
+
+    @property
+    def num_outputs(self) -> int:
+        return self._num_outputs
+
+    @property
+    def batch_shape(self):
+        """
+        This is a batch shape from an I/O perspective. For a model with `m` outputs, a `test_batch_shape x q x d`-shaped input `X`
+        to the `posterior` method returns a Posterior object over an output of
+        shape `broadcast(test_batch_shape, model.batch_shape) x q x m`.
+        """
+        return torch.Size([])
