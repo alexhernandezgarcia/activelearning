1a3bec12063355cbf849f036120f38f884a7182c
nikita-0209 <nikitasaxena0209@gmail.com>
2023-01-05 14:09:31 -0500

    add botorch acuisition class

--- proxy/acquisition.py
+++ proxy/acquisition.py
@@ -2,6 +2,9 @@ from gflownet.proxy.base import Proxy
 import numpy as np
 import os
 import torch
+from .botorch_models import ProxyBotorchUCB
+from botorch.acquisition.monte_carlo import qUpperConfidenceBound
+from botorch.sampling import SobolQMCNormalSampler
 
 
 class DropoutRegressor(Proxy):
@@ -15,12 +18,19 @@ class DropoutRegressor(Proxy):
             raise FileNotFoundError
 
     def __call__(self, inputs):
+        """
+        Args:
+            inputs: proxy-compatible input tensor
+        Returns:
+            vanilla rewards (with no power/boltzmann) transformation
+
+        """
         self.model.train()
         with torch.no_grad():
-            outputs = self.regressor.forward_with_uncertainty(
+            mean, std, var = self.regressor.forward_with_uncertainty(
                 inputs, self.num_dropout_samples
             )
-        return outputs
+        return mean, std, var
 
 
 class UCB(DropoutRegressor):
@@ -29,10 +39,26 @@ class UCB(DropoutRegressor):
         self.kappa = kappa
 
     def __call__(self, inputs):
-        outputs = super().__call__(inputs)
-        mean = np.mean(outputs, axis=1)
-        std = np.std(outputs, axis=1)
+        mean, std, _ = super().__call__(inputs)
         score = mean + self.kappa * std
         score = torch.Tensor(score)
         score = score.unsqueeze(1)
         return score
+
+
+class BotorchUCB(UCB):
+    def __init__(self, regressor, num_dropout_samples, model_path, sampler, kappa):
+        super().__init__(regressor, num_dropout_samples, model_path, kappa)
+        self.model = ProxyBotorchUCB(regressor, self.num_dropout_samples)
+        self.sampler = SobolQMCNormalSampler(
+            num_samples=sampler.num_samples,
+            seed=sampler.seed,
+            resample=sampler.resample,
+        )
+
+    def __call__(self, inputs):
+        UCB = qUpperConfidenceBound(
+            model=self.model, beta=self.kappa, sampler=self.sampler
+        )
+        acq_values = UCB(inputs)
+        return acq_values
