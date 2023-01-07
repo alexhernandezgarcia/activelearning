33bd2abad8655b129ce09544be6682ba0de84fce
nikita-0209 <nikitasaxena0209@gmail.com>
2023-01-06 19:00:00 -0500

    partial correction to botorhc-mes

--- proxy/acquisition.py
+++ proxy/acquisition.py
@@ -67,6 +67,11 @@ class UCB(DropoutRegressor):
         self.kappa = kappa
 
     def __call__(self, inputs):
+        """
+        Args
+            inputs: batch x obs_dim
+        Returns:
+            score of dim (n_samples,), i.e, ndim=1"""
         self.load_model()
         # TODO: Remove once PR38 is merged to gfn
         inputs = self.preprocess_data(inputs)
@@ -84,19 +89,20 @@ class BotorchUCB(UCB):
         self, regressor, num_dropout_samples, model_path, device, kappa, sampler
     ):
         super().__init__(regressor, num_dropout_samples, model_path, device, kappa)
-        self.sampler = SobolQMCNormalSampler(
-            num_samples=sampler.num_samples,
-            seed=sampler.seed,
-            resample=sampler.resample,
-        )
+        self.sampler_config = sampler
 
     def load_model(self):
         super().load_model()
         self.model = ProxyBotorchUCB(self.regressor, self.num_dropout_samples)
+        self.sampler = SobolQMCNormalSampler(
+            sample_shape=torch.Size([self.sampler_config.num_samples]),
+            seed=self.sampler_config.seed,
+        )
 
     def __call__(self, inputs):
         # TODO: Remove once PR38 is merged to gfn
         inputs = self.preprocess_data(inputs)
+        self.load_model()
         UCB = qUpperConfidenceBound(
             model=self.model, beta=self.kappa, sampler=self.sampler
         )
