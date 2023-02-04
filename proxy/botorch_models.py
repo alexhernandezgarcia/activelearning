8cac3a0deaed7d85c1c1abf99c8e5e5c1c041225
nikita-0209 <nikitasaxena0209@gmail.com>
2023-02-04 16:32:02 -0500

    make covar non diagonal

--- proxy/botorch_models.py
+++ proxy/botorch_models.py
@@ -87,17 +87,6 @@ class MultifidelityOracleModel(Model):
                 idx_fid = torch.where(fid_tensor == fid)[0]
                 var[idx_fid] = self.oracle[fid].sigma ** 2
             # candidate set
-            # if torch.all(torch.eq(fid_tensor, self.n_fid - 1)):
-            #     # posterior max samples generated from candidate set
-            #     var = torch.ones((X.shape[0])) * self.oracle[0].sigma ** 2
-            #     # check shape of scores
-            # else:
-            #     # posterior of candidate set
-            #     # mean = torch.zeros(X.shape[0])
-            #     var = torch.zeros(X.shape[0])
-            #     for fid in range(self.n_fid):
-            #         idx_fid = torch.where(fid_tensor == fid)[0]
-            #         var[idx_fid] = self.oracle[fid].sigma ** 2
             mean = scores
             covar = torch.diag(var)
         # batch and projected batch
@@ -105,20 +94,20 @@ class MultifidelityOracleModel(Model):
             # combination of target and original fidelity test set
             #  X = 32 x 1 x 2 x 3
             var_curr_fidelity = torch.zeros(X.shape[0], 1)
+            var_joint = torch.zeros(X.shape[0], 1)
             states = X[:, :, 0, :-1].squeeze(-2)
             states = states.squeeze(-2)
             scores = self.oracle[self.n_fid - 1](states)
             scores = scores.unsqueeze(-1)
-            # mean_curr_fidelity = torch.zeros(X.shape[0], 1)
             fid_tensor = X[:, :, 0, -1]
             for fid in range(self.n_fid):
                 idx_fid = torch.where(fid_tensor == fid)[0]
                 var_curr_fidelity[idx_fid] = self.oracle[fid].sigma ** 2
-            # mean = torch.stack((mean_curr_fidelity, mean_max_fidelity), dim=2)
+                var_joint[idx_fid] = self.oracle[fid].sigma * self.oracle[self.n_fid - 1].sigma
             mean = torch.stack((scores, scores), dim=2)
             var_max_fidelity = self.oracle[self.n_fid - 1].sigma ** 2
             covar = [
-                torch.diag(torch.FloatTensor([var_curr_fidelity[i], var_max_fidelity]))
+                torch.FloatTensor([[var_curr_fidelity[i], var_joint[i]], [var_joint[i], var_max_fidelity]])
                 for i in range(X.shape[0])
             ]
             covar = torch.stack(covar, axis=0)
@@ -130,14 +119,11 @@ class MultifidelityOracleModel(Model):
             state_tensor = state_tensor.squeeze(-2)
             scores = self.oracle[self.n_fid - 1](state_tensor)
             mean = scores.unsqueeze(-1)
-            # mean = torch.ones((X.shape[0], 1))
             var = torch.ones((X.shape[0], 1))
             for fid in range(self.n_fid):
                 idx_fid = torch.where(fid_tensor == fid)[0]
-                # state_fid = state_tensor[idx_fid]
                 var[idx_fid] = self.oracle[fid].sigma ** 2
             # posterior of max samples
-            # var = torch.ones((X.shape[0], 1)) * self.oracle[0].sigma ** 2
             covar = [torch.diag(var[i]) for i in range(X.shape[0])]
             covar = torch.stack(covar, axis=0)
 
--- proxy/mf_acquisition.py
+++ proxy/mf_acquisition.py
@@ -10,6 +10,88 @@ import numpy as np
 from botorch.models.cost import AffineFidelityCostModel
 from botorch.acquisition.cost_aware import InverseCostWeightedUtility
 
+from gpytorch.functions import inv_quad
+CLAMP_LB = 1.0e-8
+from botorch.models.utils import check_no_nans
+
+class myGIBBON(qMultiFidelityLowerBoundMaxValueEntropy):
+
+    def _compute_information_gain(
+        self, X, mean_M, variance_M, covar_mM):
+        posterior_m = self.model.posterior(
+            X, observation_noise=True, posterior_transform=self.posterior_transform
+        )
+        mean_m = self.weight * posterior_m.mean.squeeze(-1)
+        # batch_shape x 1
+        variance_m = posterior_m.variance.clamp_min(CLAMP_LB).squeeze(-1)
+        # batch_shape x 1
+        check_no_nans(variance_m)
+
+        # get stdv of noiseless variance
+        stdv = variance_M.sqrt()
+        # batch_shape x 1
+
+        # define normal distribution to compute cdf and pdf
+        normal = torch.distributions.Normal(
+            torch.zeros(1, device=X.device, dtype=X.dtype),
+            torch.ones(1, device=X.device, dtype=X.dtype),
+        )
+
+        # prepare max value quantities required by GIBBON
+        mvs = torch.transpose(self.posterior_max_values, 0, 1)
+        # 1 x s_M
+        normalized_mvs = (mvs - mean_m) / stdv
+        # batch_shape x s_M
+
+        cdf_mvs = normal.cdf(normalized_mvs).clamp_min(CLAMP_LB)
+        pdf_mvs = torch.exp(normal.log_prob(normalized_mvs))
+        ratio = pdf_mvs / cdf_mvs
+        check_no_nans(ratio)
+
+        # prepare squared correlation between current and target fidelity
+        rhos_squared = torch.pow(covar_mM.squeeze(-1), 2) / (variance_m * variance_M)
+        # batch_shape x 1
+        check_no_nans(rhos_squared)
+
+        # calculate quality contribution to the GIBBON acqusition function
+        inner_term = 1 - rhos_squared * ratio * (normalized_mvs + ratio)
+        acq = -0.5 * inner_term.clamp_min(CLAMP_LB).log()
+        # average over posterior max samples
+        acq = acq.mean(dim=1).unsqueeze(0)
+
+        if self.X_pending is None:
+            # for q=1, no replusion term required
+            return acq
+
+        X_batches = torch.cat(
+            [X, self.X_pending.unsqueeze(0).repeat(X.shape[0], 1, 1)], 1
+        )
+        # batch_shape x (1 + m) x d
+        # NOTE: This is the blocker for supporting posterior transforms.
+        # We would have to process this MVN, applying whatever operations
+        # are typically applied for the corresponding posterior, then applying
+        # the posterior transform onto the resulting object.
+        V = self.model(X_batches)
+        # Evaluate terms required for A
+        A = V.lazy_covariance_matrix[:, 0, 1:].unsqueeze(1)
+        # batch_shape x 1 x m
+        # Evaluate terms required for B
+        B = self.model.posterior(
+            self.X_pending,
+            observation_noise=True,
+            posterior_transform=self.posterior_transform,
+        ).mvn.covariance_matrix.unsqueeze(0)
+        # 1 x m x m
+
+        # use determinant of block matrix formula
+        V_determinant = variance_m - inv_quad(B, A.transpose(1, 2)).unsqueeze(1)
+        # batch_shape x 1
+
+        # Take logs and convert covariances to correlations.
+        r = V_determinant.log() - variance_m.log()
+        r = 0.5 * r.transpose(0, 1)
+        return acq + r
+
 
 class MultiFidelityMESOracle(Proxy):
     def __init__(
@@ -22,7 +104,7 @@ class MultiFidelityMESOracle(Proxy):
         self.n_fid = n_fid
         self.oracle = oracle
         self.env = env
-        self.load_candidate_set()
+        candidate_set = self.load_candidate_set()
         self.load_model()
         self.get_cost_utility()
         if self.is_model_oracle:
@@ -30,15 +112,24 @@ class MultiFidelityMESOracle(Proxy):
         else:
             # TODO: implement for proxy, replace one-hot-encoding
             raise NotImplementedError("MF-GIBBON project() not adapted to proxy")
+        self.qMES = myGIBBON(
+            self.model,
+            candidate_set=candidate_set,
+            project=self.project,
+            cost_aware_utility=self.cost_aware_utility,
+        )
 
     def get_cost_utility(self):
         if self.is_model_oracle:
             # target fidelity is 2.0
             cost_model = AffineFidelityCostModel(
-                fidelity_weights={-1: 2.0}, fixed_cost=5.0
+                fidelity_weights={2: 2.0}, fixed_cost=5.0
             )
             self.cost_aware_utility = InverseCostWeightedUtility(cost_model=cost_model)
         else:
+            # need to chnage subset of fidelity and target fidelity
+            # subset is last three colums
+            # target is [0 0 1] assuming fid=2 is the best fidelity
             raise NotImplementedError
             # cost_model = AffineFidelityCostModel(fidelity_weights={len(): 1.0}, fixed_cost=5.0)
 
@@ -53,7 +144,8 @@ class MultiFidelityMESOracle(Proxy):
         state = [self.env.readable2state(sample) for sample in samples]
         state_oracle = self.env.statebatch2proxy(state)
         # Tensor for grid
-        self.candidate_set = state_oracle
+        # self.candidate_set = state_oracle
+        return state_oracle
 
     def load_model(self):
         if self.is_model_oracle:
@@ -83,11 +175,12 @@ class MultiFidelityMESOracle(Proxy):
             states = torch.FloatTensor(states)
         # add extra dimension for q
         states = states.unsqueeze(-2)
-        qMES = qMultiFidelityLowerBoundMaxValueEntropy(
-            self.model,
-            candidate_set=self.candidate_set,
-            project=self.project,
-            cost_aware_utility=self.cost_aware_utility,
-        )
-        mes = qMES(states)
+        # qMES = qMultiFidelityLowerBoundMaxValueEntropy(
+        #     self.model,
+        #     candidate_set=self.candidate_set,
+        #     project=self.project,
+        #     cost_aware_utility=self.cost_aware_utility,
+        # )
+        
+        mes = self.qMES(states)
         return mes
