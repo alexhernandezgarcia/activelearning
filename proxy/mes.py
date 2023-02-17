e443fe8a2510b582b100b4f9ddd69096ac81f917
nikita-0209 <nikitasaxena0209@gmail.com>
2023-02-17 18:18:56 -0500

    wip: kg acqf does not work with states dimension

--- proxy/mes.py
+++ proxy/mes.py
@@ -20,138 +20,10 @@ from botorch.models.utils import check_no_nans
 from .botorch_models import FidelityCostModel
 from abc import abstractmethod
 import matplotlib.pyplot as plt
-from torch.nn.utils.rnn import pad_sequence
-
-# class MultiFidelityMES(Proxy):
-#     def __init__(
-#         self,
-#         num_dropout_samples,
-#         logger,
-#         env,
-#         is_model_oracle,
-#         n_fid,
-#         fixed_cost,
-#         regressor=None,
-#         oracle=None,
-#         data_path=None,
-#         user_defined_cost=True,
-#         **kwargs
-#     ):
-#         super().__init__(**kwargs)
-#         # self.data_path = data_path
-#         self.logger = logger
-#         self.is_model_oracle = is_model_oracle
-#         self.n_fid = n_fid #base
-#         # self.oracle = oracle
-#         self.env = env
-#         self.user_defined_cost = user_defined_cost #base
-#         self.fixed_cost = 1e-6 #base
-#         # TODO: remove member variable candidate_set as
-#         self.candidate_set = self.load_candidate_set() #base
-#         self.load_model(regressor, num_dropout_samples) #base but check args
-#         self.get_cost_utility()
-#         if self.is_model_oracle:
-#             self.project = self.project_oracle_max_fidelity
-#         else:
-#             self.project = self.project_proxy_max_fidelity
-#         # base
-#         self.qMES = qMultiFidelityLowerBoundMaxValueEntropy(
-#             self.model,
-#             candidate_set=self.candidate_set,
-#             project=self.project,
-#             cost_aware_utility=self.cost_aware_utility,
-#         )
-
-#     def get_cost_utility(self):
-#         if self.user_defined_cost:
-#             # dict[fidelity] = cost
-#             self.cost_aware_utility = FidelityCostModel(
-#                 fidelity_weights=self.env.fidelity_costs,
-#                 fixed_cost=self.fixed_cost,
-#                 device=self.device,
-#             )
-#         elif self.is_model_oracle:
-#             # target fidelity is 2.0
-#             cost_model = AffineFidelityCostModel(
-#                 fidelity_weights={-1: 2.0}, fixed_cost=self.fixed_cost
-#             )
-#             self.cost_aware_utility = InverseCostWeightedUtility(cost_model=cost_model)
-#         else:
-#             cost_model = AffineFidelityCostModel(
-#                 fidelity_weights={-1: 1.0, -2: 0.0, -3: 0.0}, fixed_cost=self.fixed_cost
-#             )
-#             self.cost_aware_utility = InverseCostWeightedUtility(cost_model=cost_model)
-
-#     def load_candidate_set(self):
-#         if self.is_model_oracle:
-#             path = self.data_path
-#             data = pd.read_csv(path, index_col=0)
-#             samples = data["samples"]
-#         else:
-#             path = self.logger.data_path.parent / Path("data_train.csv")
-#             data = pd.read_csv(path, index_col=0)
-#             samples = data["samples"]
-#         state = [self.env.readable2state(sample) for sample in samples]
-#         state_proxy = self.env.statebatch2proxy(state)
-#         if isinstance(state_proxy, torch.Tensor):
-#             return state_proxy.to(self.device)
-#         return torch.FloatTensor(state_proxy).to(self.device)
-
-#     def load_model(self, regressor, num_dropout_samples):
-#         if self.is_model_oracle:
-#             self.model = MultifidelityOracleModel(
-#                 self.oracle, self.n_fid, device=self.device
-#             )
-#         else:
-#             self.model = MultiFidelityProxyModel(
-#                 regressor, num_dropout_samples, self.n_fid, device=self.device
-#             )
-
-#     def project_oracle_max_fidelity(self, states):
-#         input_dim = states.ndim
-#         max_fid = torch.ones((states.shape[0], 1), device=self.device).long() * (
-#             self.n_fid - 1
-#         )
-#         if input_dim == 3:
-#             states = states[:, :, :-1]
-#             max_fid = max_fid.unsqueeze(1)
-#             states = torch.cat([states, max_fid], dim=2)
-#         elif input_dim == 2:
-#             states = states[:, :-1]
-#             states = torch.cat([states, max_fid], dim=1)
-#         return states
-
-#     def project_proxy_max_fidelity(self, states):
-#         input_dim = states.ndim
-#         max_fid = torch.zeros((states.shape[0], self.n_fid), device=self.device).long()
-#         max_fid[:, -1] = 1
-#         if input_dim == 3:
-#             states = states[:, :, : -self.n_fid]
-#             max_fid = max_fid.unsqueeze(1)
-#             states = torch.cat([states, max_fid], dim=2)
-#         elif input_dim == 2:
-#             states = states[:, : -self.n_fid]
-#             states = torch.cat([states, max_fid], dim=1)
-#         return states
-
-#     def __call__(self, states):
-#         if isinstance(states, np.ndarray):
-#             states = torch.FloatTensor(states)
-#         # add extra dimension for q
-#         states = states.unsqueeze(-2)
-#         # qMES = qMultiFidelityLowerBoundMaxValueEntropy(
-#         #     self.model,
-#         #     candidate_set=self.candidate_set,
-#         #     project=self.project,
-#         #     cost_aware_utility=self.cost_aware_utility,
-#         # )
-
-#         mes = self.qMES(states)
-#         return mes
 
 
-class MultiFidelityMES(Proxy):
-    def __init__(self, env, cost_type, fixed_cost, logger, gibbon=True, **kwargs):
+class MultiFidelity(Proxy):
+    def __init__(self, env, cost_type, fixed_cost, logger, **kwargs):
         super().__init__(**kwargs)
         self.env = env
         self.cost_type = cost_type
@@ -159,23 +31,6 @@ class MultiFidelityMES(Proxy):
         self.logger = logger
         self.n_fid = len(self.env.fidelity_costs)
         self.candidate_set = self.load_candidate_set()
-        cost_aware_utility = self.get_cost_utility()
-        model = self.load_model()
-        if gibbon == True:
-            self.qMES = qMultiFidelityLowerBoundMaxValueEntropy(
-                model,
-                candidate_set=self.candidate_set,
-                project=self.project,
-                cost_aware_utility=cost_aware_utility,
-            )
-        else:
-            self.qMES = qMultiFidelityMaxValueEntropy(
-                model,
-                candidate_set=self.candidate_set,
-                project=self.project,
-                cost_aware_utility=cost_aware_utility,
-                gibbon=False,
-            )
 
     @abstractmethod
     def project(self, states):
@@ -224,14 +79,36 @@ class MultiFidelityMES(Proxy):
             states = torch.tensor(states, dtype=self.float, device=self.device)
         # add extra dimension for q
         states = states.unsqueeze(-2)
-        mes = self.qMES(states)
+        mes = self.acqf(states)
         return mes
 
     def plot_context_points(self, **kwargs):
         pass
 
 
-class ProxyMultiFidelityMES(MultiFidelityMES):
+class MES(MultiFidelity):
+    def __init__(self, gibbon, **kwargs):
+        super().__init__(**kwargs)
+        cost_aware_utility = self.get_cost_utility()
+        model = self.load_model()
+        if gibbon == True:
+            self.acqf = qMultiFidelityLowerBoundMaxValueEntropy(
+                model,
+                candidate_set=self.candidate_set,
+                project=self.project,
+                cost_aware_utility=cost_aware_utility,
+            )
+        else:
+            self.acqf = qMultiFidelityMaxValueEntropy(
+                model,
+                candidate_set=self.candidate_set,
+                project=self.project,
+                cost_aware_utility=cost_aware_utility,
+                gibbon=False,
+            )
+
+
+class ProxyMultiFidelityMES(MES):
     def __init__(self, regressor, num_dropout_samples, **kwargs):
         self.regressor = regressor
         self.num_dropout_samples = num_dropout_samples
@@ -278,7 +155,7 @@ class ProxyMultiFidelityMES(MultiFidelityMES):
         return state_proxy
 
 
-class OracleMultiFidelityMES(MultiFidelityMES):
+class OracleMultiFidelityMES(MES):
     def __init__(self, data_path, oracle, **kwargs):
         self.data_path = data_path
         self.oracle = oracle
@@ -324,7 +201,7 @@ class OracleMultiFidelityMES(MultiFidelityMES):
         )
 
 
-class GaussianProcessMultiFidelityMES(MultiFidelityMES):
+class GaussianProcessMultiFidelityMES(MES):
     def __init__(self, regressor, **kwargs):
         self.regressor = regressor
         super().__init__(**kwargs)
