b12f4a9702c0de3996840d085385f87b653a7817
nikita-0209 <nikitasaxena0209@gmail.com>
2023-05-03 01:55:55 -0400

    add config oracle and config ppo

--- activelearning.py
+++ activelearning.py
@@ -18,7 +18,7 @@ from utils.eval_al_round import evaluate
 import pickle
 
 
-@hydra.main(config_path="./config", config_name="mf_rosenbrock")
+@hydra.main(config_path="./config", config_name="ppo_test")
 def main(config):
     if config.logger.logdir.root != "./logs":
         os.chdir(config.logger.logdir.root)
