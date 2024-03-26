e1032181efd5efa642045afcae4c26308a21537e
Christina <ginihumer96@yahoo.de>
2024-03-26 06:39:34 -0400

    config gflownet

--- my_package/sampler/sampler.py
+++ my_package/sampler/sampler.py
@@ -1,8 +1,8 @@
-from abc import abstractmethod
+from abc import abstractmethod, ABC
 import torch
 
 
-class Sampler:
+class Sampler(ABC):
     def __init__(self, surrogate):
         self.surrogate = surrogate
 
