97a754170caee6d048ce008cb0fa5c717b4621c3
Christina <ginihumer96@yahoo.de>
2024-06-10 10:24:43 -0400

    init ocp tests

--- tests/dataset/test_grid.py
+++ tests/dataset/test_grid.py
@@ -2,17 +2,20 @@ import pytest
 from activelearning.dataset.grid import GridData
 import torch
 
+
 @pytest.fixture
 def grid_data_1():
     X_data = torch.tensor([[0, 0], [0, 1], [1, 0]])
     y_data = torch.tensor([2.0, 1.5, 0.7])
     return GridData(grid_size=10, X_data=X_data, y_data=y_data)
 
+
 @pytest.fixture
 def grid_data_2():
     X_data = torch.tensor([[1, 0], [2, 1], [1, 7]])
     y_data = torch.tensor([2.1, 0.5, 0.9])
     return GridData(grid_size=3, X_data=X_data, y_data=y_data)
+
 
 @pytest.mark.parametrize(
     "grid_data",
@@ -24,3 +27,47 @@ def grid_data_2():
 def test__grid_data__initializes_properly(grid_data, request):
     grid_data = request.getfixturevalue(grid_data)
     assert True
+
+
+@pytest.mark.parametrize(
+    "states, states2proxy",
+    [
+        (
+            torch.Tensor([[0, 0], [5, 5], [10, 10]]),
+            torch.Tensor([[-1.0, -1.0], [0.0, 0.0], [1.0, 1.0]]),
+        ),
+    ],
+)
+def test__grid_data__proxy(states, states2proxy):
+    data = GridData(grid_size=10, X_data=states, y_data=None)
+    x = data[:]
+    assert torch.equal(states2proxy, x)
+
+
+@pytest.mark.parametrize(
+    "score, score_norm",
+    [
+        (
+            torch.Tensor([0.8709, 171.1192, 273.2218, 174.8875]),
+            torch.Tensor(
+                [
+                    0.0,
+                    0.6251064538955688,
+                    1.0,
+                    0.6389426589012146,
+                ]
+            ),
+        ),
+    ],
+)
+def test__grid_data__target_normalization(score, score_norm):
+    X_data = torch.tensor([[0, 0], [0, 1], [1, 0]])
+    data = GridData(
+        grid_size=10,
+        X_data=X_data,
+        y_data=score,
+        normalize_scores=True,
+        float=torch.float32,
+    )
+    _, y = data[:]
+    assert torch.equal(y, score_norm)
---
+++ tests/dataset/test_ocp.py
@@ -0,0 +1,47 @@
+import pytest
+import torch
+from activelearning.dataset.ocp import OCPDatasetHandler
+
+
+@pytest.fixture
+def ocp_dataset_handler_base():
+    ocp_checkpoint_path = "/network/scratch/a/alexandre.duval/ocp/runs/4648581/checkpoints/best_checkpoint.pt"
+    dataset_path = "/network/scratch/a/alexandre.duval/ocp/runs/4657270/deup_dataset"
+    dataset_handler = OCPDatasetHandler(
+        ocp_checkpoint_path, dataset_path, float_precision=32
+    )
+    return dataset_handler
+
+
+@pytest.fixture
+def ocp_dataset_handler_train_split():
+    ocp_checkpoint_path = "/network/scratch/a/alexandre.duval/ocp/runs/4648581/checkpoints/best_checkpoint.pt"
+    dataset_path = "/network/scratch/a/alexandre.duval/ocp/runs/4657270/deup_dataset"
+    dataset_handler = OCPDatasetHandler(
+        ocp_checkpoint_path,
+        dataset_path,
+        train_fraction=0.1,
+        float_precision=32,  # use train_fraction to create a train-test split from the training dataset
+    )
+    return dataset_handler
+
+
+@pytest.mark.parametrize(
+    "ocp_dataset_handler, train_shape, test_shape",
+    [
+        (
+            "ocp_dataset_handler_base",
+            torch.Size([40593, 352]),
+            torch.Size([27017, 352]),
+        ),
+        (
+            "ocp_dataset_handler_train_split",
+            torch.Size([4059, 352]),
+            torch.Size([36534, 352]),
+        ),
+    ],
+)
+def test__ocp_data__init(ocp_dataset_handler, train_shape, test_shape, request):
+    ocp_dataset_handler = request.getfixturevalue(ocp_dataset_handler)
+    assert ocp_dataset_handler.train_data.shape == train_shape
+    assert ocp_dataset_handler.test_data.shape == test_shape
