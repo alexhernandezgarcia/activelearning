bd26487ff05e606fcf9ec721bce7c9615dd94aad
Alex <alexhg@e.email>
2025-04-25 11:25:37 +0900

    AcquisitionProxy: allow passing None acquisition and implement set_acquisition

--- activelearning/sampler/proxy.py
+++ activelearning/sampler/proxy.py
@@ -1,9 +1,23 @@
+from typing import Optional
+
 from gflownet.proxy.base import Proxy
+
+from activelearning.acquisition.acquisition import Acquisition
 
 
 class AcquisitionProxy(Proxy):
-    def __init__(self, acquisition, **kwargs):
+    def __init__(self, acquisition: Optional[Acquisition] = None, **kwargs):
         super().__init__(**kwargs)
+        self.acquisition = acquisition
+
+    def set_acquisition(self, acquisition: Acquisition):
+        """Sets the Acquisition function of the proxy.
+
+        Parameters
+        ----------
+        acquisition : Acquisition
+            An Acquisition instance defining the acquisition function.
+        """
         self.acquisition = acquisition
 
     def __call__(self, states):
